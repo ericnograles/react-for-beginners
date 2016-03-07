498a0be4bee97bac22eed9a14c54e8fcc1457974
Eric Nograles <ENograles@352inc.com>
2016-03-07 07:53:48 -0500

    Adding the Login container to the router would be nice

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 
-
 import Home from '../Home';
 import Sample from '../Sample';
+import Login from '../Login';
 
 export default class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={Home}></Route>
+        <Route path="/login" component={Login}></Route>
         <Route path="/sample" component={Sample}></Route>
         <Route path="*" component={Home}></Route>
       </Router>
